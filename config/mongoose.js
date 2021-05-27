b710eeb3d99ff647badbfec7f095c61ca799b490
Shubham Gupta <sg350348@gmail.com>
2021-05-27 07:56:50 +0530

    Bug is fixed in deployment

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://sg350:XK6QamadFiyP9yOy@cluster0.hhfth.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+// mongoose.connect(
+//   "mongodb+srv://sg350:XK6QamadFiyP9yOy@cluster0.hhfth.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+//   { useNewUrlParser: true, useUnifiedTopology: true }
+// );
 
-// mongoose.connect("mongodb://localhost/PDF");
+mongoose.connect("mongodb://localhost/PDF");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
